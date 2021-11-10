ed0727b009a134660c072ce34e674e5f806b43b0
kendal <kendalnix@gmail.com>
2021-11-11 00:34:29 +0100

    From teito

--- js/funciones.js
+++ js/funciones.js
@@ -1,10 +1,8 @@
 function calcula(num) {
-<<<<<<< HEAD
 	var i=9;
 	var total =num+i;
+	for (i=9; i>0; i--){
+		document.write ("El número vale: " + i);
+	}
 	return total;
-=======
-    var i=9;
-    return num+i;
->>>>>>> 44d0f34a0017d8388873b2f9f7c5bcae80cc35b1
 } 

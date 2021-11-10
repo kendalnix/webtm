36d4d300c23eab0efd6e36e1f21c409257542f9e
kendal <kendalnix@gmail.com>
2021-11-11 00:50:37 +0100

    udjs

--- js/funciones.js
+++ js/funciones.js
@@ -2,7 +2,7 @@ function calcula(num) {
 	var i=9;
 	var total =num+i;
 	for (i=9; i>0; i--){
-		document.write ("El número vale lo que baga: " + i);
+		document.write ("El número asdsadvale lo que baga: " + i);
 	}
 	return total;
 } 

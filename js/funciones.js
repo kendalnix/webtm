d607380d9fe5d7fdba9edd6e510e878937844c29
kendal <kendalnix@gmail.com>
2021-11-11 00:50:04 +0100

    teita

--- js/funciones.js
+++ js/funciones.js
@@ -2,7 +2,7 @@ function calcula(num) {
 	var i=9;
 	var total =num+i;
 	for (i=9; i>0; i--){
-		document.write ("El número vale: " + i);
+		document.write ("El número vale lo que baga: " + i);
 	}
 	return total;
 } 
